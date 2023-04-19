27b73787f3314b75435f24ef18c1309a69aa941f
mostafiz181 <dev.mostafizurrahman@gmail.com>
2023-04-19 15:11:43 +0600

    class-1 install react router firebase daisyui

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDp-cL6OjZ55usI-hhoTly3xKvfVZ616ag",
+  authDomain: "auth-firebase-tailwind-6253a.firebaseapp.com",
+  projectId: "auth-firebase-tailwind-6253a",
+  storageBucket: "auth-firebase-tailwind-6253a.appspot.com",
+  messagingSenderId: "30137717931",
+  appId: "1:30137717931:web:5d5af2ae3a1ccb7bf813ee"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
